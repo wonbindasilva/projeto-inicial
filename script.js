1cbcfd0570aa7ba34690a8c135d13e1debe11340
wonbindasilva <bloomhw4@gmail.com>
2024-03-21 09:06:35 -0300

    formulario

--- script.js
+++ script.js
@@ -3,8 +3,27 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 var nome = document.getElementById("nome");
 var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var log = document.getElementById("log");
+var num = document.getElementById("num");
+var comp = document.getElementById("comp");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var uf = document.getElementById("uf");
 
 function alertar(event){
     //alert("Você clicou no botão!!!!" + event);
-    saida.innerText = "Nome: " + nome.value;
+    saida.innerText = "Nome: " + nome.value +
+            "\n Email: " + email.value +
+            "\n Telefone: " + telefone.value +
+            "\n CEP: " + cep.value +
+            "\n Logradouro: " + log.value +
+            "\n Numero: " + num.value +
+            "\n Complemento: " + comp.value +
+            "\n Bairro: " + bairro.value +
+            "\n Cidade: " + cidade.value +
+            "\n Estado:" + uf.value;
+
 }
