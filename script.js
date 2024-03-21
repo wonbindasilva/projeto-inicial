32c5d4dcdcb9fa109b667931844474dfad1283b4
wonbindasilva <bloomhw4@gmail.com>
2024-03-21 08:07:29 -0300

    formulario

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+event.preventDefault();
 var botao = document.getElementById("botao");
 botao = addEventListener("click", alertar);
+var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
 
-function alertar(){
-    alert("Você clicou no botão!!!!");
+function alertar(event){
+    //alert("Você clicou no botão!!!!" + event);
+    saida.innerText = "Nome: " + nome.value;
 }
