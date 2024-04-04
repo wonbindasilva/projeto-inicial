aa0b5be560e868f66c4c50d78361fa571a228709
wonbindasilva <bloomhw4@gmail.com>
2024-04-04 11:03:27 -0300

    buscar cep

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var saida = document.getElementById("saida-de-dados");
 var email = document.getElementById("email");
 var telefone = document.getElementById("telefone");
 var cep = document.getElementById("cep");
-var log = document.getElementById("log");
-var num = document.getElementById("num");
+var logradouro = document.getElementById("logradouro");
+var numero = document.getElementById("numero");
 var comp = document.getElementById("comp");
 var bairro = document.getElementById("bairro");
 var cidade = document.getElementById("cidade");
@@ -18,18 +18,27 @@ function alertar(event){
 
     const url = `http://viacep.com.br/ws/${cep.value}/json`;
     fetch(url)
-    .then(resposta=>resposta.json())
-    .then(dados=>alert(dados.bairro))
+        .then (function (resposta) {
+            return resposta.json()
+    })
+        .then(function (dados){
+            logradouro.value = dados.logradouro;
+            bairro.value = dados.bairro;
+            cidade.value = dados.localidade;
+            uf.value = dados.uf;
+    })
 
+}
+
+function saidaDeDados(){
     saida.innerText = "Nome: " + nome.value +
             "\n Email: " + email.value +
             "\n Telefone: " + telefone.value +
             "\n CEP: " + cep.value +
-            "\n Logradouro: " + log.value +
-            "\n Numero: " + num.value +
+            "\n Logradouro: " + logradouro.value +
+            "\n Numero: " + numero.value +
             "\n Complemento: " + comp.value +
             "\n Bairro: " + bairro.value +
             "\n Cidade: " + cidade.value +
             "\n Estado:" + uf.value;
-
 }
